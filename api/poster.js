2300ded5dc6e13a2829e48696ff1f75728198d81
Burnuk <bilgehanyk2@gmail.com>
2026-03-04 06:19:02 +0100

    Delete api directory

--- api/poster.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const { imdb } = req.query;
-  if (!imdb) return res.status(400).json({ error: "no imdb" });
-  const r = await fetch(`https://www.omdbapi.com/?i=${imdb}&apikey=e55d496a`);
-  const data = await r.json();
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.json({ poster: data.Poster && data.Poster !== "N/A" ? data.Poster : null });
-}
